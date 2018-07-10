7d377d269bae7f42284de991b589b25c6d418640
GarrettLevine <garrlevine@gmail.com>
2018-07-10 08:22:22 -0400

    progress?

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,26 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import '@storybook/addon-console';
+// import './normalize.scss';
+// import './default.scss';
+// import '../src/main.scss';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context('../src', true, /\.story\.js$/);
+
+addDecorator(story => (
+  <div style={{
+    padding: 20,
+    overflow: 'auto',
+    height: '100vh',
+    width: '100%',
+  }}>
+    {story()}
+  </div>
+))
+
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach((filename) => req(filename));
+  // You can require as many stories as you need.
 }
 
 configure(loadStories, module);
--- webpack.config.js
+++ webpack.config.js
@@ -48,3 +48,5 @@ const config = {
         ],
     },
 }
+
+module.exports = config;
